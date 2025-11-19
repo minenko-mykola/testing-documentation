5ef3460f493a34156a0604dc815e1c0c3312ffa4
minenko-mykola <minenkomykola444@gmail.com>
2025-11-19 09:07:48 +0200

    17 try

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {Calculator} from "./components";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Calculator></Calculator>
+);
